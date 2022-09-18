31534aae602b0cee8454e49547cf757c8ad5bb0c
Spillmaker <github@spillmaker.no>
2022-09-18 21:20:22 +0200

    Updated buildscripts

--- .build/build.sh
+++ .build/build.sh
@@ -1,14 +1,11 @@
 # Buildscript for mac
 
-rm -r ../dist/*
-mkdir ../dist
-cp ../src/* ../dist/
-# Upload to Classic
-rm -r /Applications/World\ of\ Warcraft/_classic_/Interface/AddOns/Bagnon_Currencies
+# Create Addon-folder if it doesnt exist in Classic
 mkdir /Applications/World\ of\ Warcraft/_classic_/Interface/AddOns/Bagnon_Currencies
-cp ../dist/* /Applications/World\ of\ Warcraft/_classic_/Interface/AddOns/Bagnon_Currencies
+# Upload to Classic
+rsync -av --exclude=".*" ../. /Applications/World\ of\ Warcraft/_classic_/Interface/AddOns/Bagnon_Currencies
 
-# Upload to Retail
-rm -r /Applications/World\ of\ Warcraft/_retail_/Interface/AddOns/Bagnon_Currencies
+# Create Addon-folder if it doesnt exist in Retail
 mkdir /Applications/World\ of\ Warcraft/_retail_/Interface/AddOns/Bagnon_Currencies
-cp ./dist/* /Applications/World\ of\ Warcraft/_retail_/Interface/AddOns/Bagnon_Currencies
+# Upload to Retail
+rsync -av --exclude=".*" ../. /Applications/World\ of\ Warcraft/_retail_/Interface/AddOns/Bagnon_Currencies
