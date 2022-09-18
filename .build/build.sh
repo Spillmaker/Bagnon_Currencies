5f4b0226111431af6d0dbbf62dfb804824a8e458
Spillmaker <github@spillmaker.no>
2022-09-18 16:34:28 +0200

    More structural changes to make CI easier.

--- .build/build.sh
+++ .build/build.sh
@@ -1,13 +1,12 @@
 # Buildscript for mac
 
-rm -r ./dist/*
-mkdir ./dist
-cp ./src/* ./dist/
-cp ./LICENSE ./dist/LICENSE
+rm -r ../dist/*
+mkdir ../dist
+cp ../src/* ../dist/
 # Upload to Classic
 rm -r /Applications/World\ of\ Warcraft/_classic_/Interface/AddOns/Bagnon_Currencies
 mkdir /Applications/World\ of\ Warcraft/_classic_/Interface/AddOns/Bagnon_Currencies
-cp ./dist/* /Applications/World\ of\ Warcraft/_classic_/Interface/AddOns/Bagnon_Currencies
+cp ../dist/* /Applications/World\ of\ Warcraft/_classic_/Interface/AddOns/Bagnon_Currencies
 
 # Upload to Retail
 rm -r /Applications/World\ of\ Warcraft/_retail_/Interface/AddOns/Bagnon_Currencies
